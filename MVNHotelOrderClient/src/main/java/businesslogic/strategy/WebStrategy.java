b0154c3e7bb3364a919433e3547088aeb5d4c8cd
Linwenye <419766878@qq.com>
2016-11-22 23:45:02 +0800

    更改计算策略

--- MVNHotelOrderClient/src/main/java/businesslogic/strategy/WebStrategy.java
+++ MVNHotelOrderClient/src/main/java/businesslogic/strategy/WebStrategy.java
@@ -1,9 +1,12 @@
 package businesslogic.strategy;
 
+import java.util.Iterator;
 import java.util.List;
 
+import businesslogic.discount.DiscountCalController;
 import businesslogicservice.DiscountGetService;
 import vo.DiscountVO;
+import vo.DiscountVO_hotel;
 import vo.DiscountVO_web;
 import vo.OrderInputVO;
 /**
@@ -11,19 +14,40 @@ import vo.OrderInputVO;
  * @author LinWenye
  *
  */
-//调用DiscountDealService得到符合条件的策略，计算出最优
 
 public class WebStrategy {
 	private List<DiscountVO_web> strategyList;
 
 	public WebStrategy(OrderInputVO orderInput) {
 		
-		DiscountGetService disDealService = new MockDiscount("web");//桩程序
+//		DiscountGetService disDealService = new MockDiscount("web");//桩程序
+//		
+//		strategyList = disDealService.getSuitableDiscount_web(orderInput);
 		
-		strategyList = disDealService.getSuitableDiscount_web(orderInput);
+		DiscountGetService discount = new DiscountCalController();
+		strategyList = discount.getSuitableDiscount_web(orderInput);
 	}
-//计算得到最优
+/**
+ * 计算得到最优
+ * @return 最优策略
+ */
 	public DiscountVO_web calBest() {
-		return (DiscountVO_web) strategyList.get(0);
+//		return (DiscountVO_web) strategyList.get(0);
+
+		double min = 1.0;
+		DiscountVO_web res = new DiscountVO_web(0, null, min, null, null, 0);
+		
+		Iterator iter = strategyList.iterator();
+	
+		while (iter.hasNext()) {
+			DiscountVO_web discountVO_web = (DiscountVO_web) iter.next();
+			if (discountVO_web.discount < min) {
+
+				min = discountVO_web.discount;
+				res=discountVO_web;
+				
+			}
+		}
+		return res;
 	}
 }
--- MVNHotelOrderClient/src/main/java/businesslogicservice/DiscountGetService.java
+++ MVNHotelOrderClient/src/main/java/businesslogicservice/DiscountGetService.java
@@ -13,7 +13,11 @@ import vo.OrderInputVO;
  * change: 分离接口 ，使策略计算时不调用冗余接口。
  */
 public interface DiscountGetService {
-
+		/**
+		 * 
+		 * @param orderInput
+		 * @return
+		 */
 		public List<DiscountVO_hotel> getSuitableDiscount_hotel(OrderInputVO orderInput);
 		public List<DiscountVO_web> getSuitableDiscount_web(OrderInputVO orderInput);
 
