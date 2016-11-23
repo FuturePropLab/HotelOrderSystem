47a1bfc4d516fc671ec5c92d54722b0824a4552c
Linwenye <419766878@qq.com>
2016-11-23 20:27:05 +0800

    更改上个提交

---
+++ MVNHotelOrderClient/src/main/java/businesslogic/strategy/Strategy_hotelType.java
@@ -0,0 +1,12 @@
+package businesslogic.strategy;
+/**
+ * 
+ * @author LWY
+ *
+ */
+public enum Strategy_hotelType {
+	Birthday,
+	OrderMore,
+	CooperateEnterprise,
+	SpecificPeriod
+}
---
+++ MVNHotelOrderClient/src/main/java/businesslogic/strategy/Strategy_webType.java
@@ -0,0 +1,11 @@
+package businesslogic.strategy;
+/**
+ * 
+ * @author LWY
+ *
+ */
+public enum Strategy_webType {
+	OrderMore,
+	VIP_district,
+	Level
+}
