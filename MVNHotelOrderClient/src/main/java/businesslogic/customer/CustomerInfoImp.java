bdf34faac2ffc08dba1357bd3b4effffd4ec4a92
returncyy <1226550411@qq.com>
2016-11-28 13:03:50 +0800

    account里的接口实现

---
+++ MVNHotelOrderClient/src/main/java/businesslogic/customer/CustomerInfoImp.java
@@ -0,0 +1,21 @@
+package businesslogic.customer;
+
+import java.util.List;
+
+import businesslogic.account.CustomerInfo;
+import vo.CustomerSearchVO;
+import vo.CustomerVO;
+
+public class CustomerInfoImp implements CustomerInfo{
+
+	public List<CustomerVO> searchCustomer(CustomerSearchVO customerSearchVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public CustomerVO getCustomerDetail(String customer_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
