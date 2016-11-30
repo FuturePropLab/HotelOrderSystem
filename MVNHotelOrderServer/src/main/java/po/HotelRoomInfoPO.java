a14bdbbb77c11f5c7f7b2244f91ccf450c55e2a4
wshwbluebird <wshwbluebird@outlook.com>
2016-12-01 01:07:31 +0800

    酒店 5个数据库

--- MVNHotelOrderServer/src/main/java/po/HotelRoomInfoPO.java
+++ MVNHotelOrderServer/src/main/java/po/HotelRoomInfoPO.java
@@ -1,5 +1,45 @@
 package po;
 
-public class HotelRoomInfoPO {
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import tools.HotelRoomInfo;
+import tools.TypeRoomInfo;
+
+@Entity
+public class HotelRoomInfoPO {
+	private String hotelID;
+	private String hotelName;
+	private List<TypeRoomInfo> typeRoomInfo;
+	
+	public HotelRoomInfoPO(HotelRoomInfo hotelRoomInfo) {
+		this.hotelID = hotelRoomInfo.getHotelID();
+		this.hotelName = hotelRoomInfo.getHotelName();
+		this.typeRoomInfo= hotelRoomInfo.getTypeRoomInfo();
+	}
+	public HotelRoomInfoPO() {
+		super();
+	}
+	
+	@Id	
+	public String getHotelID() {
+		return hotelID;
+	}
+	public void setHotelID(String hotelID) {
+		this.hotelID = hotelID;
+	}
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public List<TypeRoomInfo> getTypeRoomInfo() {
+		return typeRoomInfo;
+	}
+	public void setTypeRoomInfo(List<TypeRoomInfo> typeRoomInfo) {
+		this.typeRoomInfo = typeRoomInfo;
+	}
 }
