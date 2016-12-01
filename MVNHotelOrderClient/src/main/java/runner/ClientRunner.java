ddab3d3e7323285005e27bdb4acf9c2ee0281b04
wshwbluebird <wshwbluebird@outlook.com>
2016-12-01 19:55:12 +0800

    文件读localhost

--- MVNHotelOrderClient/src/main/java/runner/ClientRunner.java
+++ MVNHotelOrderClient/src/main/java/runner/ClientRunner.java
@@ -1,5 +1,10 @@
 package runner;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
@@ -10,11 +15,8 @@ import java.util.List;
 import dataservice.HotelDataService;
 import po.HotelPO;
 import rmi.RemoteHelper;
-<<<<<<< Updated upstream
-=======
 import tools.HotelAddress;
 import tools.HotelRoomInfo;
->>>>>>> Stashed changes
 import tools.TypeRoomInfo;
 
 public class ClientRunner {
@@ -26,11 +28,18 @@ public class ClientRunner {
 
 	}
 	
-	private void linkToServer() {
+	private void linkToServer(){
 		try {
+			File file = new File("TxtData/port.txt");
+			InputStreamReader reader = new InputStreamReader(new FileInputStream(
+					file), "UTF-8");
+			BufferedReader br = new BufferedReader(reader);
+			String str = br.readLine();
+			String port = str.trim();
+			System.out.println(port);
 			remoteHelper = RemoteHelper.getInstance();
 			System.out.println("ready to link");
-			remoteHelper.setRemote(Naming.lookup("rmi://localhost:8888/DataRemoteObject"));
+			remoteHelper.setRemote(Naming.lookup("rmi://"+port+":8888/DataRemoteObject"));
 			System.out.println("linked");
 
 		} catch (MalformedURLException e) {
@@ -38,6 +47,9 @@ public class ClientRunner {
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		} catch (NotBoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
