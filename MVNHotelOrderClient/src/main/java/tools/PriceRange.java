b671a86c4f89027f448d4336689699c1cc7cf2e1
wshwbluebird <wshwbluebird@outlook.com>
2016-12-02 13:13:16 +0800

    接口好没??

--- MVNHotelOrderClient/src/main/java/tools/PriceRange.java
+++ MVNHotelOrderClient/src/main/java/tools/PriceRange.java
@@ -1,12 +1,18 @@
 package tools;
 
+import java.io.Serializable;
+
 import Exception.OutOfBoundsException;
 
 /**
  *  价格的范围，从x到y
  *  @author zjy
  */
-public class PriceRange {
+public class PriceRange implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7441593943480401110L;
 	double lowest;
 	double higest;
 	
@@ -23,4 +29,22 @@ public class PriceRange {
 		this.lowest=lowest;
 		this.higest=higest;
 	}
+
+	public double getLowest() {
+		return lowest;
+	}
+
+	public void setLowest(double lowest) {
+		this.lowest = lowest;
+	}
+
+	public double getHigest() {
+		return higest;
+	}
+
+	public void setHigest(double higest) {
+		this.higest = higest;
+	}
+	
+	
 }
