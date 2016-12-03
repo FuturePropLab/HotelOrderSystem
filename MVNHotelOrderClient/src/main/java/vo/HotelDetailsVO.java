3ddb3b79ed230fa45efb59e9fb9ac40e4ac1078b
wshwbluebird <wshwbluebird@outlook.com>
2016-12-03 15:56:46 +0800

    ??

--- MVNHotelOrderClient/src/main/java/vo/HotelDetailsVO.java
+++ MVNHotelOrderClient/src/main/java/vo/HotelDetailsVO.java
@@ -22,4 +22,31 @@ public class HotelDetailsVO {
 	public HotelRoomInfoVO hotelRoomInfoVO;
 	public HotelFacility hotelFacility;  //mod bt wsw
 	public HotelAddress  hotelAssess;  //mod by wsw
+	
+	/**
+	 * 
+	 * @param hotelID 酒店ID
+	 * @param hotelName 酒店名称
+	 * @param hotelImage 酒店首页的图片
+	 * @param star 酒店星级
+	 * @param hotelAddress 酒店地址
+	 * @param hotelDiscribtionsVO 酒店简介信息
+	 * @param hotelRoomInfoVO 酒店房间信息
+	 * @param hotelFacilityVO 酒店设施服务信息
+	 * @param hotelAssessVO 酒店评价信息
+	 */
+	public HotelDetailsVO(String hotelID, String hotelName, URI hotelImage, Star star, HotelAddress hotelAddress,
+			HotelDiscribtionsVO hotelDiscribtionsVO, HotelRoomInfoVO hotelRoomInfoVO, HotelFacility hotelFacilityVO,
+			HotelAddress hotelAssessVO) {
+		super();
+		this.hotelID = hotelID;
+		this.hotelName = hotelName;
+		this.hotelImage = hotelImage;
+		this.star = star;
+		this.hotelAddress = hotelAddress;
+		this.hotelDiscribtionsVO = hotelDiscribtionsVO;
+		this.hotelRoomInfoVO = hotelRoomInfoVO;
+		this.hotelFacility = hotelFacilityVO;
+		this.hotelAssess = hotelAssessVO;
+	}
 }
