b36829de404ba3bdab0177f68252a2f46b35a71d
wshwbluebird <wshwbluebird@outlook.com>
2016-12-04 12:30:59 +0800

    增加了面向room界面层的 接口注释

--- MVNHotelOrderClient/src/main/java/businesslogicservice/RoomManageService.java
+++ MVNHotelOrderClient/src/main/java/businesslogicservice/RoomManageService.java
@@ -8,15 +8,55 @@ import tools.RoomType;
 import vo.RoomDescriptionVO;
 
 public interface RoomManageService {
+	/**
+	 *获得改种房间类型的信息
+	 * @param hotel_id
+	 * @param roomType
+	 * @return
+	 */
 	public RoomDescriptionVO  getRoomInfo(String hotel_id, RoomType roomType); 
-		
+	
+	/**
+	 * 存储房间类型信息
+	 * @param hotelID
+	 * @param roomDescriptionVO
+	 * @return
+	 */
 	public ResultMessage_Room saveRoomInfo(String hotelID ,RoomDescriptionVO roomDescriptionVO);
 	
+	/**
+	 * 获得该酒店该类型所有房间号
+	 * @param hotelID
+	 * @param roomType
+	 * @return
+	 */
 	public List<String> getAllRoomByType(String hotelID , RoomType roomType);
 	
+	/**
+	 * 获得该酒店该类型在规定时间内可用的房间号
+	 * @param hotelID
+	 * @param roomType
+	 * @param begin
+	 * @param end
+	 * @return
+	 */
 	public List<String>  getAvaiableRoomBytime (String hotelID , RoomType roomType , Date begin,Date end );
 	
+	/**
+	 * 获得该酒店该类型所有的房间个数
+	 * @param hotelID
+	 * @param roomType
+	 * @return
+	 */
 	public int getAllNumberByType(String hotelID , RoomType roomType);
 	
+	/**
+	 * 获得该酒店该类型在规定时间内可用的房间个数
+	 * @param hotelID
+	 * @param roomType
+	 * @param begin
+	 * @param end
+	 * @return
+	 */
 	public int getAvaiableNumberByTime(String hotelID , RoomType roomType , Date begin,Date end);
 }
--- MVNHotelOrderClient/src/main/java/businesslogicservice/RoomSingleService.java
+++ MVNHotelOrderClient/src/main/java/businesslogicservice/RoomSingleService.java
@@ -5,14 +5,47 @@ import java.util.Date;
 import tools.ResultMessage_Room;
 import tools.RoomType;
 
+/**
+ * 
+ * @author wshwbluebird
+ *
+ */
 public interface RoomSingleService {
 	
+	/**
+	 * 增加房间
+	 * @param hotelID
+	 * @param RoomNO
+	 * @param roomType
+	 * @return
+	 */
 	public ResultMessage_Room addSingleRoom(String hotelID, String RoomNO, RoomType roomType);
 	
+	/**
+	 * 删除房间
+	 * @param hotelID
+	 * @param roomNO
+	 * @return
+	 */
 	public ResultMessage_Room deleteSingleRoom (String hotelID,String roomNO);
 	
-	public ResultMessage_Room addDisable (String hotelID,String roomNO , Date negin , Date end);
+	/**
+	 * 将房间一段时间设为不可用
+	 * @param hotelID
+	 * @param roomNO
+	 * @param begin
+	 * @param end
+	 * @return
+	 */
+	public ResultMessage_Room addDisable (String hotelID,String roomNO , Date begin , Date end);
 	
-	public ResultMessage_Room deleteDisable (String hotelID,String roomNO , Date negin);
+	/**
+	 * 删除 不可用的记录
+	 * @param hotelID
+	 * @param roomNO
+	 * @param begin
+	 * @return
+	 */
+	public ResultMessage_Room deleteDisable (String hotelID,String roomNO , Date begin);
 	
 }
