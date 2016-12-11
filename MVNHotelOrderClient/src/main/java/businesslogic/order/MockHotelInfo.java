d564068f85802b1e7d0b17579ef8432845a8f524
returncyy <1226550411@qq.com>
2016-12-11 21:02:44 +0800

    测试计算ordervalue

--- MVNHotelOrderClient/src/main/java/businesslogic/order/MockHotelInfo.java
+++ MVNHotelOrderClient/src/main/java/businesslogic/order/MockHotelInfo.java
@@ -16,7 +16,7 @@ public class MockHotelInfo implements HotelInfo{
 
 	public HotelbriefVO getHotelInfo(String hotelID) {
 		HotelbriefVO hotelInfoVO = new HotelbriefVO();
-		hotelInfoVO.averagePrice=100;
+		if(hotelID=="123"){hotelInfoVO.averagePrice=100;
 		hotelInfoVO.hotelAddress=new HotelAddress("city", "district", "businessCircle", "addressDetail");
 		hotelInfoVO.hotelID="hotelID";
 		hotelInfoVO.hotelName="hotelName";
@@ -27,6 +27,21 @@ public class MockHotelInfo implements HotelInfo{
 			e.printStackTrace();
 		}
 		hotelInfoVO.star=Star.one;
+		return hotelInfoVO;}
+		else if(hotelID=="124"){
+			hotelInfoVO.averagePrice=100;
+			hotelInfoVO.hotelAddress=new HotelAddress("city", "district", "businessCircle", "addressDetail");
+			hotelInfoVO.hotelID="hotelID";
+			hotelInfoVO.hotelName="hotelName";
+			try {
+				hotelInfoVO.mark=new Mark(3);
+				hotelInfoVO.priceRange=new PriceRange(100, 100);
+			} catch (OutOfBoundsException e) {
+				e.printStackTrace();
+			}
+			hotelInfoVO.star=Star.one;
+			return hotelInfoVO;
+		}
 		return hotelInfoVO;
 	}
 
