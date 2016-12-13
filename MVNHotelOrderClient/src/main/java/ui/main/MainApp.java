7b6993025e2112a8a2292c516815b35ccde7442f
wshwbluebird <wshwbluebird@outlook.com>
2016-12-13 23:51:53 +0800

    ??

--- MVNHotelOrderClient/src/main/java/ui/main/MainApp.java
+++ MVNHotelOrderClient/src/main/java/ui/main/MainApp.java
@@ -3,6 +3,7 @@ package ui.main;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import runner.ClientRunner;
+import ui.main.MainUI;
 /**
  * 整个软件的入口，包含main方法，启动软件的类
  * @author zjy
@@ -13,6 +14,7 @@ public class MainApp extends Application{
 	@Override
 	public void start(Stage primaryStage){
 		ClientRunner cr = new ClientRunner();
+		
 		MainUI mainUI=new MainUI(primaryStage);
 	}
 
