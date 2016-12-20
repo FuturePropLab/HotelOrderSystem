ae32438bcd62dd08a96a0c88778e993e832a9c71
FinalZJY <805642794@qq.com>
2016-12-20 10:35:01 +0800

    debug 登陆界面线程BUG

--- MVNHotelOrderClient/src/main/java/ui/login/LoginController.java
+++ MVNHotelOrderClient/src/main/java/ui/login/LoginController.java
@@ -6,9 +6,9 @@ import java.rmi.RemoteException;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXSpinner;
 import com.jfoenix.controls.JFXTextField;
-import com.jfoenix.svg.SVGGlyphLoader;
 
 import businesslogic.customer.CustomerSignupController;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -18,8 +18,6 @@ import serviceFactory.LoginServiceUtil;
 import tools.AccountType;
 import tools.ResultMessage_LoginCheck;
 import tools.ResultMessage_signUp;
-import ui.customer.ClientInfoController;
-import ui.discount.WebDiscountController;
 import ui.hotel.HotelDetailController;
 import ui.hotel.HotelSearchController;
 import ui.main.FullLayoutController;
@@ -72,68 +70,71 @@ public class LoginController extends FullLayoutController{
 		loginButton.setVisible(false);
 		spinner.setVisible(true);
 		
-		new Thread(()->{
-			try {
-				ResultMessage_LoginCheck result;
-				String accountTy = this.accountType.getValue();
-				AccountType accountType  = null;
-				if(accountTy.equals(accountTypes[0])) accountType = AccountType.Customer;
-				else if(accountTy.equals(accountTypes[1]))  accountType = AccountType.Hotel;
-				else if(accountTy.equals(accountTypes[2]))  accountType = AccountType.Web;
-				else if(accountTy.equals(accountTypes[3]))  accountType = AccountType.Administor;
-				else    return;
-				
-				result = LoginServiceUtil.getLoginService().login(username.getText(), password.getText(),accountType);
-				if(result.equals(ResultMessage_LoginCheck.Success)){
+		Platform.runLater(new Runnable(){
+			@Override
+			public void run() {
+				try {
+					ResultMessage_LoginCheck result;
+					String accountTy = accountType.getValue();
+					AccountType accountType  = null;
+					if(accountTy.equals(accountTypes[0])) accountType = AccountType.Customer;
+					else if(accountTy.equals(accountTypes[1]))  accountType = AccountType.Hotel;
+					else if(accountTy.equals(accountTypes[2]))  accountType = AccountType.Web;
+					else if(accountTy.equals(accountTypes[3]))  accountType = AccountType.Administor;
+					else    return;
 					
-					saveUsernameUtil.saveinfo(username.getText().trim(), accountTy);
-					String accountID  =  LoginServiceUtil.getLoginService().getLogState().accountID;
-					rootLayoutController.changeFullLayout(null);
-					rootLayoutController.changeGuid("../guid/GuideUI.fxml");
-					
-					AccountType testType  = LoginServiceUtil.getLoginService().getLogState().accountType;
-					System.out.println(testType);
-					
-					if( testType  == AccountType.Customer){			
-						rootLayoutController.changeDetails("../hotel/HotelSearch.fxml");
-						HotelSearchController controller =
-								(HotelSearchController) rootLayoutController.getDetailsController();
+					result = LoginServiceUtil.getLoginService().login(username.getText(), password.getText(),accountType);
+					if(result.equals(ResultMessage_LoginCheck.Success)){
 						
-					}else if(testType == AccountType.Hotel){
-						System.out.println("Brfore into Detail Hotel : "+accountID);
-						rootLayoutController.changeDetails("../hotel/HotelDetail.fxml");
-						HotelDetailController hotelDetailController  =
-								(HotelDetailController) rootLayoutController.getDetailsController();
-						hotelDetailController.initValue(accountID);
-					}else if(testType == AccountType.Web){
-						rootLayoutController.changeDetails("../webdesign/creditCharge.fxml");				
+						saveUsernameUtil.saveinfo(username.getText().trim(), accountTy);
+						String accountID  =  LoginServiceUtil.getLoginService().getLogState().accountID;
+						rootLayoutController.changeFullLayout(null);
+						rootLayoutController.changeGuid("../guid/GuideUI.fxml");
 						
+						AccountType testType  = LoginServiceUtil.getLoginService().getLogState().accountType;
+						System.out.println(testType);
+						
+						if( testType  == AccountType.Customer){			
+							rootLayoutController.changeDetails("../hotel/HotelSearch.fxml");
+							HotelSearchController controller =
+									(HotelSearchController) rootLayoutController.getDetailsController();
+							
+						}else if(testType == AccountType.Hotel){
+							System.out.println("Brfore into Detail Hotel : "+accountID);
+							rootLayoutController.changeDetails("../hotel/HotelDetail.fxml");
+							HotelDetailController hotelDetailController  =
+									(HotelDetailController) rootLayoutController.getDetailsController();
+							hotelDetailController.initValue(accountID);
+						}else if(testType == AccountType.Web){
+							rootLayoutController.changeDetails("../webdesign/creditCharge.fxml");				
+							
+						}
 					}
+					else if (result.equals(ResultMessage_LoginCheck.InvalidUsername)) {
+						System.out.println("Invalid Username");
+						Dialogs.showMessage("不存在这个用户名");
+					}
+					else if (result.equals(ResultMessage_LoginCheck.InvalidPassword)) {
+						System.out.println("Invalid Password");
+						Dialogs.showMessage("密码错误");
+					}
+					else if (result.equals(ResultMessage_LoginCheck.SystemError)) {
+						System.err.println("login fail:SystemError");
+					}else if(result.equals(ResultMessage_LoginCheck.hasOn)){
+						Dialogs.showMessage("该用户已经在线");
+					}
+				} catch (RemoteException e) {
+					System.out.println("connect error");
+					Dialogs.showMessage("阿欧","网络连接没有成功耶");
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}finally {
+					loginButton.setVisible(true);
+					spinner.setVisible(false);
 				}
-				else if (result.equals(ResultMessage_LoginCheck.InvalidUsername)) {
-					System.out.println("Invalid Username");
-					Dialogs.showMessage("不存在这个用户名");
-				}
-				else if (result.equals(ResultMessage_LoginCheck.InvalidPassword)) {
-					System.out.println("Invalid Password");
-					Dialogs.showMessage("密码错误");
-				}
-				else if (result.equals(ResultMessage_LoginCheck.SystemError)) {
-					System.err.println("login fail:SystemError");
-				}else if(result.equals(ResultMessage_LoginCheck.hasOn)){
-					Dialogs.showMessage("该用户已经在线");
-				}
-			} catch (RemoteException e) {
-				System.out.println("connect error");
-				Dialogs.showMessage("阿欧","网络连接没有成功耶");
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}finally {
-				loginButton.setVisible(true);
-				spinner.setVisible(false);
 			}	
-		}).start();
+		});
 	}
 	
 	@FXML
